968a6c2a45bf78478ebbf47168aed460339b19cd
vijayakoppineedi <vijaya.koppineedi@gmail.com>
2015-01-27 11:15:10 -0500

    modifications to the gene functionality

--- sites/all/modules/custom/custom_i5k/theme/templates/tripal_feature_detail_transcript.tpl.php
+++ sites/all/modules/custom/custom_i5k/theme/templates/tripal_feature_detail_transcript.tpl.php
@@ -1,22 +1,40 @@
-<h1>Detailed Transcript View</h1>
+<link rel="stylesheet" href="/sites/all/themes/i5k/css/jquery-ui.css">
+<script src="/sites/all/themes/i5k/js/jquery-ui.js"></script> 
+<script>
+  jQuery(function() {
+    jQuery("#accordion").accordion();
+  });
+</script>
+<?php
+$feature = $features[0];
+?>  
+<h1>Detailed Transcript View - <?php print $feature->subject_id->uniquename; ?></h1>
 
+<div id="accordion">
 <?php
 //echo "<pre> variables::: "; print_r($features[0]); echo "</pre>";
 
 //$feature = $variables['node']->feature;
+//echo "<pre> Hello: "; print_r($_SESSION['node']); echo "</pre>";
 
-//echo "<pre>"; print_r($features[0]); echo "</pre>";
-if(!empty($features[0])) {
-  $feature = $features[0];
+if(!empty($feature)) {
+  
   $options = array('return_array' => 1);
+  //echo "<prE>"; print_r($feature); echo "</pre>";
+  
   $feature = chado_expand_var($feature, 'table', 'analysisfeature', $options);
   $analyses = $feature->subject_id->analysisfeature;
+  
+  $src_values = array('feature_id' => $feature->subject_id->featureloc->feature_id->srcfeature_id->feature_id);
+  $srcfeature = chado_generate_var('analysisfeature', $src_values, $options);           
+  
+//echo "<pre> hello "; print_r($srcfeature); echo "</pre>";  
   
   // Dbxref, expand the feature object to include the records from the feature_dbxref table
   $options = array('return_array' => 1);
   $dx_feature = chado_expand_var($feature, 'table', 'feature_dbxref', $options);  
   $feature_dbxrefs = $dx_feature->subject_id->feature_dbxref;  
-  //echo "<prE>"; print_r($feature_dbxrefs); echo "</pre>";
+  
   $references = '';	
   if (count($feature_dbxrefs) > 0 ) {    
     foreach ($feature_dbxrefs as $feature_dbxref) {    
@@ -28,7 +46,7 @@ if(!empty($features[0])) {
     }
   }    
 
-  // Analyses 
+  // Analyses - if and else statements cause the anaylses having different array formats.
   if(count($analyses) == 2) {  
     $analysis_name = '';
       foreach($analyses as $analysis) {  
@@ -44,19 +62,32 @@ if(!empty($features[0])) {
 	  //$options['target'] = '_blank';
       $analysis_name .= l($a_name, "node/" . $analyses->analysis_id->nid)."<bR>";  
     }   
-
+  }
+  
+  // Source name comes under analysis 
+  if(isset($srcfeature[0]->analysis_id) && count($srcfeature[0]->analysis_id)) {
+    $src_analysis = $srcfeature[0]->analysis_id;
+    $a_name = $src_analysis->name;
+    $analysis_name .= "Source: ". l($a_name, "node/" . $src_analysis->nid);
   }
 
   //User comment
   $featureprop_id = $feature->subject_id->feature_id;  
   $select = array('feature_id' => $featureprop_id, 'type_id' => 85);
   $columns = array('value', 'feature_id');
-  $featureprop = chado_select_record('featureprop', $columns, $select);
-  $user_comment = '';
-  if(!empty($featureprop[0]->value))
-    $user_comment = 'Note: '.$featureprop[0]->value;
-
-  // the $headers array is an array of fields to use as the colum headers.
+  $featureprop = chado_select_record('featureprop', $columns, $select);  
+  $featureprop = array_reverse($featureprop);
+  
+  $user_comment = ''; $i = 0;
+  foreach($featureprop as $prop_obj) {
+  if(!empty($prop_obj->value)) {
+    ($i == 0)? $user_comment .= 'Note: ':'';
+    $user_comment .= $prop_obj->value."; ";
+	$i++;
+  }	  
+  }
+   $user_comment = rtrim($user_comment, '; ');
+  // the $headers array is an array of fields to use as the column headers.
   $headers = array();
 
   // the $rows array contains an array of rows where each row is an array
@@ -104,15 +135,6 @@ if(!empty($features[0])) {
       $references
     );
 
-    // Source row
-    $rows[] = array(
-      array(
-        'data' => 'Source',
-        'header' => TRUE
-      ),
-      'Need more info'
-    );
-
     // Analysis row
     $rows[] = array(
       array(
@@ -125,7 +147,7 @@ if(!empty($features[0])) {
     // User Submitted info row
     $rows[] = array(
       array(
-        'data' => 'User Submitted info',
+        'data' => 'Annotator Comments',
         'header' => TRUE
       ),
       $user_comment
@@ -148,13 +170,17 @@ if(!empty($features[0])) {
 
   // once we have our table array structure defined, we call Drupal's theme_table()
   // function to generate the table.
+  print '<h3>Details</h3>';
+  print '<div>';
   print theme_table($table); 
+  print '</div>';
 }
 
 //Bottom table
 if(isset($data)) {
   $headers = array('Name', 'ID', 'Type', 'Reference Sequence ID', 'start', 'End', 'Strand', 'View sequence');
   $rows = array();
+  
   //Gene and mRNA data
   if(isset($features['data'])) {
     foreach($features['data'] as $key => $parent_data) {
@@ -169,8 +195,7 @@ if(isset($data)) {
 		array('data' =>$parent_data['view_sequence'], 'width' => '10%')                      
       ); 
 	} 
-  }
-  
+  }  
   foreach($data as $key => $data) {
  	
 	
@@ -201,8 +226,8 @@ if(isset($data)) {
     'header' => $headers,
     'rows' => $rows,
     'attributes' => array(
-      'id' => 'tripal_feature-detail-transcript',
-      'class' => 'tripal-data-table'
+      'id' => 'tripal_feature-fasta-detail-transcript',
+      'class' => 'tripal-data-table1'
     ),
     'sticky' => FALSE,
     'caption' => '',
@@ -210,11 +235,27 @@ if(isset($data)) {
     'empty' => '',
   );
  ?>
-<div id='popup-window'> 
-</div>
+ 
+ <?php
+ //echo "<pre>"; print_r($feature); echo "</pre>";   exit;
+   $feature_terms_text = theme('tripal_feature_terms', array('node' => $features[0], 'arg' => $feature->subject_id->feature_id));
+  if(!empty($feature_terms_text)) {  ?>
+  <h3>Annotated Terms</h3>
+  <div>
+  <?php 
+  
+  print $feature_terms_text;
+  ?>     
+  </div>
+  <?php  }   
+  ?>
 <?php 
   // once we have our table array structure defined, we call Drupal's theme_table()
   // function to generate the table.
+  print '<h3>Feature details</h3>';
+  print '<div>';
   print theme_table($table);
-}	   
-?>
+  print '</div>';
+}	 
+ ?>
+</div>
--- sites/all/modules/custom/custom_i5k/theme/templates/tripal_feature_transcripts.tpl.php
+++ sites/all/modules/custom/custom_i5k/theme/templates/tripal_feature_transcripts.tpl.php
@@ -1,6 +1,7 @@
 <?php
-//echo "<pre>"; print_r($node); echo "</pre>";
+//echo "<pre> tripalfeature: "; print_r($node); echo "</pre>";
 $feature = $node->feature;
+$_SESSION['node'] = $node;
 
 $all_relationships = $feature->all_relationships;
 //echo "<pre>"; print_r($all_relationships); echo "</pre>";
@@ -20,27 +21,45 @@ if (count($object_rels) > 0 or count($subject_rels) > 0) { ?>
       // the $headers array is an array of fields to use as the colum headers.
       // additional documentation can be found here
       // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-      $headers = array('Name', 'Type', 'Transcript Length', 'Transcript Protein', 'Detail View');
+      $headers = array('Name', 'Type', 'Transcript Length', 'Protein Length', 'Detailed View');
       
       // the $rows array contains an array of rows where each row is an array
       // of values for each column of the table in that row.  Additional documentation
       // can be found here:
       // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
       $rows = array();
-      
-      foreach ($objects as $object){	  
-	    $seqlength = '-';
-	    if(!empty($object->record->object_id->seqlen))
-		  $seqlength = $object->record->object_id->seqlen;
+  
+      foreach ($objects as $object){	
+  
+		// Below tripal function is used to get seqlen from featureloc relationship
+		$pep_values = array('object_id' => $object->record->object_id->feature_id);
+        $pep_options = array('return_array' => 1);		
+		
+        $pep_feature = chado_generate_var('feature_relationship', $pep_values, $pep_options);
+		
+	    $pep_seqlen = "NA";	
+        foreach($pep_feature as $key => $pep_subject) {
+          if($pep_subject->subject_id->type_id->cvterm_id == PEP_TYPE_ID) {
+	        $pep_seqlen = $pep_subject->subject_id->seqlen;
+	      }
+        }				 
+		  
+		// Transcript length is the cdna length coz cdna residues showing for mRNA
+		$cdna_seq_values = array('uniquename' => $object->record->object_id->uniquename, 'type_id' => CDNA_TYPE_ID);	
+		$cdna_feature_seqlen = chado_generate_var('feature', $cdna_seq_values);		
+	    $seqlength = 'NA';		
+	    if(!empty($cdna_feature_seqlen->seqlen))
+		  $seqlength = $cdna_feature_seqlen->seqlen;
+		  
 		$link = '';  
-		if(isset($subject->record->subject_id->feature_id))
-		  $link = "<div class='tripal_toc_list_item'><a id='detail_transcript_view' class='tripal_toc_list_item_link detail_transcripts_link-".$subject->record->subject_id->feature_id."' href='#'>link</a></div>";
+		if(isset($object->record->object_id->feature_id))
+		  $link = "<div class='tripal_toc_list_item'><a id='detail_transcript_view' class='tripal_toc_list_item_link detail_transcripts_link-".$object->record->object_id->feature_id."' href='#'>link</a></div>";
 		  
         $rows[] = array(          
           array('data' => $object->record->object_id->uniquename, 'width' => '30%'),          
           array('data' => $object->record->object_id->type_id->name, 'width' => '10%'),
 		  $seqlength,
-		  'Need more info',
+		  $pep_seqlen,
 		  $link
         ); 
        } 
@@ -76,28 +95,43 @@ if (count($object_rels) > 0 or count($subject_rels) > 0) { ?>
       // the $headers array is an array of fields to use as the colum headers.
       // additional documentation can be found here
       // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-      $headers = array('Name', 'Type', 'Transcript Length', 'Transcript Protein', 'Detail View');
+      $headers = array('Name', 'Type', 'Transcript Length', 'Protein Length', 'Detailed View');
       
       // the $rows array contains an array of rows where each row is an array
       // of values for each column of the table in that row.  Additional documentation
       // can be found here:
       // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
       $rows = array();
-      
-      foreach ($subjects as $subject){ 	  
+
+      foreach ($subjects as $subject){	   
 	    $nid = $subject->record->object_id->nid;
+		// Below tripal function is used to get seqlen from featureloc relationship
+		$pep_values = array('object_id' => $subject->record->subject_id->feature_id);
+        $pep_options = array('return_array' => 1);		
 		
-	    $seqlength = '-';
-	    if(!empty($subject->record->subject_id->seqlen))
-		  $seqlength = $subject->record->subject_id->seqlen;
-		  //?pane=detail_transcripts&fid=".$subject->record->subject_id->feature_id."
-		  $link = "<div class='tripal_toc_list_item'><a id='detail_transcript_view' class='tripal_toc_list_item_link detail_transcripts_link-".$subject->record->subject_id->feature_id."-".$nid."' href='#'>link</a></div>";
+        $pep_feature = chado_generate_var('feature_relationship', $pep_values, $pep_options);
+		
+		//echo "poepe ".PEP_TYPE_ID;
+	    $pep_seqlen = "NA";	
+        foreach($pep_feature as $key => $pep_subject) {
+		  if($pep_subject->subject_id->type_id->cvterm_id == PEP_TYPE_ID) {
+	        $pep_seqlen = $pep_subject->subject_id->seqlen;
+	      }
+        }				
+		// Transcript length is the cdna length coz cdna residues showing for mRNA
+		$cdna_seq_values = array('uniquename' => $subject->record->subject_id->uniquename, 'type_id' => CDNA_TYPE_ID);	
+		$cdna_feature_seqlen = chado_generate_var('feature', $cdna_seq_values);		
+	    $seqlength = 'NA';		
+	    if(!empty($cdna_feature_seqlen->seqlen))
+		  $seqlength = $cdna_feature_seqlen->seqlen;
+		
+		$link = "<div class='tripal_toc_list_item'><a id='detail_transcript_view' class='tripal_toc_list_item_link detail_transcripts_link-".$subject->record->subject_id->feature_id."-".$nid."' href='#'>link</a></div>";
 		  
         $rows[] = array(
           array('data' =>$subject->record->subject_id->uniquename, 'width' => '30%'),          
           array('data' =>$subject->record->subject_id->type_id->name, 'width' => '10%'),
 		  array('data' =>$seqlength, 'width' => '10%'),  
-          'Need more info',
+          array('data' => $pep_seqlen, 'width' => '10%'),
           $link		  
         ); 
        } 
@@ -126,3 +160,5 @@ if (count($object_rels) > 0 or count($subject_rels) > 0) { ?>
      }
   }
 }
+
+
